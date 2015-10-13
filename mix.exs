6c3999f06baff8c8492cf5b1eb9b215f7c1b93d6
Franklin Chen <franklinchen@franklinchen.com>
2015-10-13 17:51:08 -0400

    Some cleanups.

--- mix.exs
+++ mix.exs
@@ -4,11 +4,13 @@ defmodule Tau.Mixfile do
   def project do
     [app: :tau,
      version: "0.0.7",
-     name: "tau",
      elixir: "~> 1.0",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps,
+     name: "tau",
      source_url: "https://github.com/FranklinChen/tau",
      homepage_url: "https://github.com/FranklinChen/tau",
-     deps: deps,
      description: "The mathematical constant tau",
      package: package
     ]
@@ -16,9 +18,9 @@ defmodule Tau.Mixfile do
 
   # Configuration for the OTP application
   #
-  # Type `mix help compile.app` for more information
+  # Type "mix help compile.app" for more information
   def application do
-    []
+    [applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
@@ -29,7 +31,7 @@ defmodule Tau.Mixfile do
   #
   #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
   #
-  # Type `mix help deps` for more examples and options
+  # Type "mix help deps" for more examples and options
   defp deps do
     [{:earmark, "~> 0.1", only: :dev},
      {:inch_ex, only: :docs},
--- test/tau_test.exs
+++ test/tau_test.exs
@@ -1,10 +1,10 @@
 defmodule TauTest do
   use ExUnit.Case
+  doctest Tau
 
   test "relationship with pi" do
     import Tau
 
     assert tau/2 == :math.pi
   end
-
 end
